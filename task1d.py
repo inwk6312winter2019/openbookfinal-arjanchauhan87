73bb8dc91986326eba81c2cddbfed5bf5865d9d8
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2019-02-15 10:26:01 -0800

    task1d file

---
+++ task1d.py
@@ -0,0 +1,11 @@
+def character_word_count(string):
+ 	 my_string = string.lower().split()
+ 	 my_dict = {}
+ 	 for item in my_string:
+   	  my_dict[item] = item.count(item)
+ 	 print(my_dict)
+
+word_count("I am that I am")
+
+
+
