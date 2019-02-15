06bee6ec7abd98c2cbe304b8f10e1ed15e8c6698
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2019-02-15 10:08:20 -0800

    file task1B

---
+++ task1B.py
@@ -0,0 +1,5 @@
+def count_the_article():
+	mylist = ["a", "the", "at", "run", "to","and","are","or","for","an","this"]
+	for x, word in enumerate(mylist):
+        for i, subwords in enumerate(word):
+        print i
